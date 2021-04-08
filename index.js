63e83470981786093ca9ff932fbebc5695fe39a8
kendofriendo <goadke@g.cofc.edu>
2021-04-08 13:39:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener( "DOMContentLoaded", function () {
+	replaceText()
+  } );
+
+function replaceText() {
+	document.getElementById("text")
+	.innerText = "This is really cool!";
+}
